e09456f6e99151c971cc9396b3cadd52e8fa399f
ValidusCM <validuscm@gmail.com>
2013-10-13 18:15:04 -0400

    Update vanir_d2vzw.mk

--- products/vanir_d2vzw.mk
+++ products/vanir_d2vzw.mk
@@ -6,11 +6,11 @@ KERNEL_SPAM := CM Kernel
 
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true \
-    ro.goo.rom=vanir-d2vzw
+    ro.goo.rom=vanir-xt907
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/boot_animations/720x1280.zip:system/media/bootanimation.zip
+    vendor/vanir/proprietary/boot_animations/540x960.zip:system/media/bootanimation.zip
 
 # Inherit common phone stuff
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
@@ -20,13 +20,13 @@ $(call inherit-product, vendor/vanir/products/cdma.mk)
 $(call inherit-product, vendor/vanir/products/beats.mk)
 
 # Inherit AOSP device configuration for GS3 VZW.
-$(call inherit-product, device/samsung/d2vzw/full_d2vzw.mk)
+$(call inherit-product, device/motorola/xt907/full_xt907.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := vanir_d2vzw
-PRODUCT_DEVICE := d2vzw
-PRODUCT_BRAND := Samsung
-PRODUCT_MANUFACTURER := Samsung
-PRODUCT_MODEL := SCH-I535
+PRODUCT_NAME := vanir_xt907
+PRODUCT_DEVICE := xt907
+PRODUCT_BRAND := motorola
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_MODEL := xt907
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2vzw TARGET_DEVICE=d2vzw BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.3.1/JZO54K/JLS36I:user/release-keys" PRIVATE_BUILD_DESC="d2vzw-user 4.3.1 JLS36I I535VRBMF1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=xt907 TARGET_DEVICE=xt907 BUILD_FINGERPRINT="Verizon/motorola/xt907:4.3.1/JZO54K/JLS36I:user/release-keys" PRIVATE_BUILD_DESC="xt907-user 4.3.1 JLS36I I535VRBMF1 release-keys"
